0eea1967923dd8d611fc90f889298a915a65e1e8
KATO Kanryu <k.kanryu@gmail.com>
2014-11-13 16:50:22 +0900

    renew routing

--- examples/routing/01_LocationService/hashbang/script.js
+++
@@ -1,5 +0,0 @@
-/// <reference path="../typings/tsd.d.ts" />
-angular.module("hashbangModule", ["mainModule"]).config(function ($locationProvider) {
-    //    $locationProvider.html5Mode({enabled:true, requireBase:false}).hashPrefix("!");
-    $locationProvider.html5Mode({ enabled: false, requireBase: false }).hashPrefix("!");
-});
