364be8618a14faf817edd61699812472cb3836c7
KATO Kanryu <k.kanryu@gmail.com>
2014-11-13 16:50:59 +0900

    renew routing

---
+++ examples/routing/01_LocationService/html5/script.ts
@@ -0,0 +1,8 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+angular.module("html5Module", ["mainModule"])
+  .config(function ($locationProvider)
+  {
+console.log($locationProvider);
+    $locationProvider.html5Mode({enabled:true, requireBase:false}).hashPrefix("!");
+  });
